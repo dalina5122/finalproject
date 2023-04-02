4051921134724ea4f5332384b4c54766525c01ba
dalina5122 <dalina5122@users.noreply.github.com>
2023-04-02 15:04:23 +0100

    side menu

--- petapp/frontend/src/components/DogsMap.vue
+++ petapp/frontend/src/components/DogsMap.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- TOP BAR -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <!-- dog icon-click to switch to cat map -->
         <a class="navbar-brand">
@@ -23,17 +24,43 @@
         </div>   
     </nav>
 
-    <div class="container">
-        <div>
-            <img loading="auto" src="/media/map.png" width="800">
-        </div>
 
-        <!-- <div>
-            <svg width="400" height="110">
-                <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
-            </svg>
-        </div> -->
+
+
+    <!-- SIDE MENU -->
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-black min-vh-50">
+
+                    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-black text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline">MENU</span>
+                    </a>
+
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item nav-link">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item nav-link">
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Pet Care</span></a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+
+            <!-- map -->
+            <div class="col-auto">
+                <img loading="auto" src="/media/map.png" width="800">
+            </div>
+        </div>
     </div>
+
+
 </template>
 
 
