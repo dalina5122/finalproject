d2f9219762e8c7bcd6a77591875d3e5997f1a058
dalina5122 <dalina5122@users.noreply.github.com>
2023-04-23 00:25:50 +0100

    models and signup

--- petapp/models.py
+++ petapp/models.py
@@ -1,39 +1,36 @@
 from django.conf import settings
 
 from django.db import models
+
 from django.contrib.auth import get_user_model
-
 CustomUser = get_user_model()
-
-# from users.models import CustomUser
 
 from django.utils import timezone
 
-class Pet(models.Model):
-    GENDER_CHOICES = [
+class Dog(models.Model):
+    GENDER_CHOICES_D = [
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other'),
     ]
 
-    picture=models.ImageField(upload_to='pet_images', blank=True, null=True)
-    age=models.IntegerField(default=0)
-    name=models.CharField(max_length=200, default=' ')
-    location=models.CharField(max_length=200, default=' ')
-    color=models.CharField(max_length=50, default=' ')
-    description=models.TextField(max_length=1000, default=' ')
-    date=models.DateTimeField(default=timezone.now)
-    breed=models.CharField(max_length=200, default=' ')
-    gender=models.CharField(max_length=1, choices=GENDER_CHOICES, default=' ')
-    # username=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    pet_ptr = models.OneToOneField('self', on_delete=models.CASCADE, parent_link=True, primary_key=True, default=None)
-
+    picture_d=models.ImageField(upload_to='pet_images', blank=True, null=True)
+    age_d=models.IntegerField(default=0)
+    name_d=models.CharField(max_length=200, default=' ')
+    location_d=models.CharField(max_length=200, default=' ')
+    color_d=models.CharField(max_length=50, default=' ')
+    description_d=models.TextField(max_length=1000, default=' ')
+    date_d=models.DateTimeField(default=timezone.now)
+    breed_d=models.CharField(max_length=200, default=' ')
+    gender_d=models.CharField(max_length=1, choices=GENDER_CHOICES_D, default=' ')
+    username=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    id=models.AutoField(primary_key=True, default=1)
 
     def __str__(self):
         """
         Display the object name when printing the object
         """
-        return self.name
+        return self.name_d
 
     def to_dict(self):
         """
@@ -41,24 +38,58 @@ class Pet(models.Model):
         """
         return{
             'id': self.id,
-            'picture': self.picture.url,
-            'age': self.age,
-            'name': self.name,
-            'location': self.location,
-            'color': self.color,
-            'description': self.description,
-            'date': self.date,
-            'breed': self.breed,
-            'gender': self.gender,
+            'picture_d': self.picture_d.url,
+            'age_d': self.age_d,
+            'name_d': self.name_d,
+            'location_d': self.location_d,
+            'color_d': self.color_d,
+            'description_d': self.description_d,
+            'date_d': self.date_d,
+            'breed_d': self.breed_d,
+            'gender_d': self.gender_d,
             'username': self.username.id,
         }
 
-    # class Meta:
-    #     abstract = True
+class Cat(models.Model):
+    GENDER_CHOICES_C = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
 
-class Dog(Pet):
-    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    picture_c=models.ImageField(upload_to='pet_images', blank=True, null=True)
+    age_c=models.IntegerField(default=0)
+    name_c=models.CharField(max_length=200, default=' ')
+    location_c=models.CharField(max_length=200, default=' ')
+    color_c=models.CharField(max_length=50, default=' ')
+    description_c=models.TextField(max_length=1000, default=' ')
+    date_c=models.DateTimeField(default=timezone.now)
+    breed_c=models.CharField(max_length=200, default=' ')
+    gender_c=models.CharField(max_length=1, choices=GENDER_CHOICES_C, default=' ')
+    username=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    id=models.AutoField(primary_key=True, default=1)
 
-class Cat(Pet):
-    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        """
+        Display the object name when printing the object
+        """
+        return self.name_c
+
+    def to_dict(self):
+        """
+        Use this to return a dictionary of the Answer object
+        """
+        return{
+            'id': self.id,
+            'picture_c': self.picture_c.url,
+            'age_c': self.age_c,
+            'name_c': self.name_c,
+            'location_c': self.location_c,
+            'color_c': self.color_c,
+            'description_c': self.description_c,
+            'date_c': self.date_c,
+            'breed_c': self.breed_c,
+            'gender_c': self.gender_c,
+            'username': self.username.id,
+        }
--- petapp/views.py
+++ petapp/views.py
@@ -4,31 +4,31 @@ from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 import json
 
-from .models import Pet
+from .models import Cat, Dog
 
 from users.models import CustomUser
 
 
-def newpet_api(request):
+def newdog_api(request):
     if request.method == "GET":
         return JsonResponse({
-            'pets': [
-                pet.to_dict()
-                for pet in Pet.objects.all()
+            'dogs': [
+                dog.to_dict()
+                for dog in Dog.objects.all()
             ]
         })
 
     if request.method == "POST":
         owner = get_object_or_404(User, id = request.user.id)
         pet=Pet.objects.create(
-            age=request.POST.get('age'),
-            name=request.POST.get('name'),
-            location=request.POST.get('location'),
-            color=request.POST.get('color'),
-            description=request.POST.get('description'),
-            date=request.POST.get('date'),
-            breed=request.POST.get('breed'),
-            gender=request.POST.get('gender'),
+            age_d=request.POST.get('age_d'),
+            name_d=request.POST.get('name_d'),
+            location_d=request.POST.get('location_d'),
+            color_d=request.POST.get('color_d'),
+            description_d=request.POST.get('description_d'),
+            date_d=request.POST.get('date_d'),
+            breed_d=request.POST.get('breed_d'),
+            gender_D=request.POST.get('gender_d'),
             user=owner
         )
-        return JsonResponse(pet.to_dict())
+        return JsonResponse(dog.to_dict())
