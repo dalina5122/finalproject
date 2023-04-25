6e18068e602b18b8c6617b60c305b5ee357055ff
dalina5122 <dalina5122@users.noreply.github.com>
2023-04-25 01:52:53 +0100

    signup working

--- users/views.py
+++ users/views.py
@@ -7,18 +7,30 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def signup(request):
+    # if request.method == 'POST':
+    #     form = SignUpForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('home')
+    # else:
+    #     form = SignUpForm()
+    # return render(request, 'signup.html', {'form': form})
+
     if request.method == 'POST':
         form = SignUpForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('home')
+            user = form.save()
+            return JsonResponse({'success': True, 'user_id': user.id})
+        else:
+            errors = form.errors.as_json()
+            return JsonResponse({'success': False, 'errors': errors})
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
-def csrf_middleware(get_response):
-    def middleware(request):
-        response = get_response(request)
-        response["X-CSRFToken"] = request.COOKIES.get("csrftoken")
-        return response
-    return middleware
+# def csrf_middleware(get_response):
+#     def middleware(request):
+#         response = get_response(request)
+#         response["X-CSRFToken"] = request.COOKIES.get("csrftoken")
+#         return response
+#     return middleware
